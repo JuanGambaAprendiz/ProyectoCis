f532c64524bd203094e6036ffde636225d26d7cd
JuanGambaAprendiz <jdgamba6@misena.edu.co>
2019-12-09 10:12:32 -0500

    Footer Versión Editada 1
    Se editaron los archivos para verificar datos del Footer (pendiente de cambios).
    
    Co-Authored-By: Luis Carlos Hernández Peña <lucashernandez067@users.noreply.github.com>

--- code/est_footer.php
+++ code/est_footer.php
@@ -1,2 +1,4 @@
-<center><p>&copy; 2019 Inventarios Muebles NGR. Todos los derechos reservados.</p>
-<p> Diseñado por: Brayan Rojas, Juan Gamba & Luis Hernandez.</p></center>
+<div style="bottom: 0; left: 0; right: 0;">
+    <center><p>&copy; 2019 Inventarios Muebles NGR. Todos los derechos reservados.</p>
+    <p> Diseñado por: Brayan Rojas, Juan Gamba & Luis Hernandez.</p></center>
+</div>
