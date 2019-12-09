10bca76d9a6e19ca0d012a4a8828482f657c0c5b
JuanGambaAprendiz <48841021+JuanGambaAprendiz@users.noreply.github.com>
2019-12-09 13:20:44 -0500

    neg_agregarRol.php Versión 1
    Se creó la capa que posee el formulario para agregar rápidamente un rol al sistema.
    La capa solo se utilizará para pruebas por una restricción en su uso implementada a causa de la necesidad de construir un módulo para implementar el rol que sea añadido.
    
    Co-Authored-By: Luis Carlos Hernández Peña <lucashernandez067@users.noreply.github.com>

---
+++ code/pre_agregarRol.php
@@ -0,0 +1,89 @@
+<?php
+    include("neg_seguridad_2.php");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+	<?php 
+		include("din_headMain.php");
+	?>
+	<title>Agregar Rol | NGR</title>
+</head>
+
+<body>
+	<!-- Barra de la izquierda -->
+	<section class="full-box cover dashboard-sideBar">
+		<div class="full-box dashboard-sideBar-bg btn-menu-dashboard"></div>
+		<div class="full-box dashboard-sideBar-ct">
+			<div class="full-box text-uppercase text-center text-titles dashboard-sideBar-title">
+				Muebles NGR <i class="zmdi zmdi-close btn-menu-dashboard"></i>
+			</div>
+			<div class="full-box dashboard-sideBar-UserInfo">
+				<figure class="full-box">
+                    <div class="form-group">
+                        <figcaption class="text-center text-titles mb-2">Rol de Usuario</figcaption>
+                        <select class="form-control" name="rol" id="rol" onchange="location = this.value">
+                            <option hidden value="" title="select" label="" selected disabled>Superadministrador</option>
+							<?php
+								include('est_select.php');
+							?>
+                        </select>
+                    </div>
+				</figure>
+			</div>
+			<!--Menú interno-->
+			<?php
+				include('est_sideBar_superAdmin.php');
+			?>
+		</div>
+	</section>
+
+	<!-- Contenido del tablero -->
+	<section class="full-box dashboard-contentPage">
+		
+		<?php
+			include("est_navbar_dashboard.php");
+		?>
+		
+		<!-- Contenido de la página -->
+		<div class="container-fluid" style="margin-top: 15px;">
+			<div class="page-header">
+			  <h1 class="text-titles"><span class="text-info">Sistema</span> Control de Inventarios - SUPERADMINISTRADOR</h1>
+			</div>
+        </div>
+        <div class="page-header">
+			  <h1 class="text-titles text-center">Agregar Rol Al Sistema</h1>
+        </div>
+        <div class="container" style="max-width: 630px;">
+            <form action="neg_agregarRol.php" method="POST" autocomplete="off">
+                <div class="form-group">
+                    <label>Nombre del Rol</label>
+                    <input type="text" class="form-control" name="rolNombre" placeholder="Nombre del rol" + required>
+                </div>
+                <center>
+					<a href="pre_roles.php" class="btn btn-danger">Cancelar</a>
+					<button type="submit" class="btn btn-primary">Agregar</button>
+				</center>
+            </form>
+		</div>
+		<?php 
+			if((isset($_REQUEST['p'])) and ($_REQUEST['p'] == 1)){			
+		?>
+			<div class="container" style=" margin-top: 7px; max-width: 630px;">
+				<center><p class="text-danger">Ya existe este rol.</p></center>
+			</div>
+		<?php 
+			}
+		?>
+		<section>
+			<br><hr>
+			<?php include("est_footer.php"); ?>
+		</section>
+	</section>
+
+</body>
+</html>
